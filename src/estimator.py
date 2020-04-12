8e738ec2e1fdd8725094457960de2da6ed623705
ardesp <belloshehu1@gmail.com>
2020-04-12 10:57:33 +0100

    challenge1_solution created and returns output_data containing both estimations required in challenge 1

--- src/estimator.py
+++ src/estimator.py
@@ -7,7 +7,7 @@ output_data = {
     "severeImpact":{}
 }
 
-
+#returns duration for estimation
 def get_duration(data):
 
     if data['periodType'] == 'months':
@@ -17,11 +17,27 @@ def get_duration(data):
     else:
         duration = data['timeToElapse']*1
     return duration
+  
+  #challenge 1 function 
+def challenge1_soluton(data):
+    #estimating the currently Infected people for both impact and severeImpact 
+    impact_currently_infected = data["reportedCases"]*10
+    severe_Impact_currently_infected = data["reportedCases"]*50
+    output_data["impact"]["currentlyInfected"] = impact_currently_infected
+    output_data["severeImpact"]["currentlyInfected"] = severe_Impact_currently_infected
+
+    #estimating the imfected people for duration based on timeToElapse for both impact and severeImpact
+    impact_infections_by_requested_time = impact_currently_infected*(2**(get_duration(data)//3))
+    severe_Impact_infections_by_requested_time = severe_Impact_currently_infected*(2**(get_duration(data)//3))
+    output_data["impact"]["infectionsByRequestedTime"] = impact_infections_by_requested_time
+    output_data["severeImpact"]["infectionsByRequestedTime"] = severe_Impact_infections_by_requested_time
+    return output_data
 
 def estimator(data):
 
   #challenge 1
-  
-  
+  result = challenge1_soluton(data)
+  return json.dumps(result, indent=2)
 
-  return json.dumps(output_data, indent=2)
+
+print(estimator(data))
